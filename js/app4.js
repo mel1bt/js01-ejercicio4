9dd2cd583f8bf07202427774dca93d4decbad5c9
mel1bt <mberrocal.tipte@gmail.com>
2016-08-25 15:49:47 -0500

    Cuarto Ejercicio

---
+++ js/app4.js
@@ -0,0 +1,5 @@
+var tiempo = parseInt(prompt("Ingrese el pago"));
+var costoMinuto = parseInt( prompt("Ingrese las horas"));
+var pago=0;
+pago = tiempo * costoMinuto;
+document.write("El pago es " + pago );
